753e0f02af47847ff78b51ba569f90832405483d
Hiroto Takahashi <elzzup.htam@i.softbank.jp>
2014-01-14 22:45:58 +0900

    update filer

--- src/Filer.java
+++ src/Filer.java
@@ -2,11 +2,9 @@ import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
 
-
 public class Filer {
 
 	Desktop desktop;
-
 	public Filer() {
 		desktop = Desktop.getDesktop();
 	}
@@ -20,17 +18,28 @@ public class Filer {
 	}
 
 	public void create(String filename) {
+		File f = new File(filename);
 		try {
-			File f = new File(filename);
 			f.createNewFile();
-		} catch (Exception e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
+	public void delete(String filename) {
+		File f = new File(filename);
+		f.delete();
+	}
+
+	public boolean move(String source, String target) {
+		File f = new File(source);
+		File tof = new File(target);
+		return f.renameTo(tof);
+	}
+
 	public static void main(String... args) {
 		Filer f = new Filer();
-		f.create("create_test.txt");
-		f.open("tes.txt");
+		String fn = "create_test.txt";
+		System.out.println(f.move (fn, "testdir\tester.txt"));
 	}
 }
